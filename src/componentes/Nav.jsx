034256f22b4f97feced99b8e27bf34b3b74b15b2
EchauriMu <edechaurimu@ittepic.edu.mx>
2024-10-03 19:45:24 -0700

    resposividad xd

--- src/componentes/Nav.jsx
+++ src/componentes/Nav.jsx
@@ -1,47 +1,66 @@
 // Nav.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../assets/Nav.css';
 
 const Nav = ({ setActiveTab }) => {
-//solo animaciones
-  let links = document.querySelectorAll("li");
-links.forEach((link, Nav) => {
-  link.addEventListener("click", () => {
-    links.forEach((item) => item.classList.remove("active"));
-
-    link.classList.add("active");
-
-  });
-});
+  useEffect(() => {
+    // Seleccionamos todos los elementos li una vez que el componente está montado
+    let links = document.querySelectorAll("li");
+    links.forEach((link) => {
+      link.addEventListener("click", () => {
+        links.forEach((item) => item.classList.remove("active"));
+        link.classList.add("active");
+      });
+    });
+    // Cleanup: elimina los event listeners cuando el componente se desmonte
+    return () => {
+      links.forEach((link) => {
+        link.removeEventListener("click", () => {});
+      });
+    };
+  }, []); // El array vacío asegura que esto se ejecute solo una vez al montar el componente
 
   return (
     <nav className="nav-fondo">
       <div className="nav-contenedor">
         <div className="nav-logo">
-          <img src="https://clubmate.fish/wp-content/uploads/2021/06/eCommerce-Icon-1.png" alt="logo" className="nav-imagen" />
+          <img
+            src="https://clubmate.fish/wp-content/uploads/2021/06/eCommerce-Icon-1.png"
+            alt="logo"
+            className="nav-imagen"
+          />
           <span className="nav-titulo">Ecommerce</span>
         </div>
 
-        <div class="navbar">
-  
-        <li class="active">
-          <a  onClick={() => setActiveTab('tab1')}> <i class="fas fa-home"></i></a>
-        </li>
-        <li>
-          <a  onClick={() => setActiveTab('tab2')}> <i class="fa-solid fa-user"></i></a>
-        </li>
-        <li>
-          <a  onClick={() => setActiveTab('tab3')}> <i class="fa-solid fa-gear"></i></a>
-        </li>
-        <li>
-          <a  onClick={() => setActiveTab('tab4')}> <i class="fa-solid fa-right-from-bracket"></i></a>
-        </li>
-
-    
-    </div>
+        <div className="navbar">
+          <li className="active" title="Inicio">
+            <a onClick={() => setActiveTab('tab1')}>
+              <i className="fas fa-home"></i>
+            </a>
+          </li>
+          <li>
+            <a onClick={() => setActiveTab('tab2')}>
+              <i className="fa-solid fa-user"></i>
+            </a>
+          </li>
+          <li>
+            <a onClick={() => setActiveTab('tab3')}>
+              <i className="fa-solid fa-gear"></i>
+            </a>
+          </li>
+          <li>
+            <a onClick={() => setActiveTab('tab4')}>
+              <i className="fa-solid fa-right-from-bracket"></i>
+            </a>
+          </li>
+        </div>
 
         <div className="nav-usuario">
-          <img src="https://scontent.ftpq2-1.fna.fbcdn.net/v/t39.30808-1/455476687_2814693292027069_8664129958700040026_n.jpg?stp=dst-jpg_s200x200&_nc_cat=107&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeFwP2IsYRMgREGX1kH4ADLTyEAMyeQVZhrIQAzJ5BVmGuhhxH9B16tkFqclA9Cf7rntyrb-wDGw6r6_A1GHgnHU&_nc_ohc=fDxckDbJE9QQ7kNvgGxToP2&_nc_ht=scontent.ftpq2-1.fna&oh=00_AYDKGitjljD4FYnUEWx5BZIwYcjT8Z27IqMP67v9EpCoig&oe=67025A37" alt="Foto de perfil" className="nav-foto" />
+          <img
+            src="https://i.pinimg.com/736x/f5/df/b4/f5dfb46f188e6b30f7bf6c81798c7c44.jpg"
+            alt="Foto de perfil"
+            className="nav-foto"
+          />
           <span className="nav-nombre">Eduardo</span>
         </div>
       </div>
